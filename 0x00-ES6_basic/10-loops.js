c0197af91d57be2ce7fd318f4f4c9b843cea6494
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-01-02 16:51:18 +0100

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) {
-    array[array.indexOf(value)] = appendString + value;
+  const arrayCopy = array;
+  for (const [idx, element] of arrayCopy.entries()) {
+    arrayCopy[idx] = appendString + element;
   }
 
-  return array;
+  return arrayCopy;
 }
