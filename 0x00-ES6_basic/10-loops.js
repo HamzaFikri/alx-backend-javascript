3d940620b7a4bd5edaf3723f6405cd7c0c4f3927
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-01-03 14:52:22 +0100

    fix error

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable guard-for-in */
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const idx of array) {
-    newArray.push(appendString + idx);
+    newArray.push(`${appendString}${idx}`);
   }
 
   return newArray;
