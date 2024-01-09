f1b34baa5681b3a2a61f3f99240744006becee08
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-01-09 01:03:10 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,8 @@
-export default function cleanSet(aSet, aStartString) {
-  if (!aSet || !aStartString || !(aSet instanceof aSet) || typeof aStartString !== 'string') {
-    return '';
-  }
-  return Array.from(aSet)
-    .filter((ele) => ele && ele.startsWith(aStartString))
-    .map((ele) => ele.replace(aStartString, ''))
-    .join('-');
+export default function cleanSet(set, startString) {
+  let result = '';
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
+    if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
 }
